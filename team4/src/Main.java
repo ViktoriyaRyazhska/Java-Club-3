a8da8c391cbb09d1e5471909a889df60811c8781
andrey-yaremko <Typicm@gmail.com>
2021-10-05 15:06:03 -0400

    team4

---
+++ team4/src/Main.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while (true){
+            System.out.println("Enter task's number");
+            switch (sc.next()){
+                case "1":{
+
+                }
+                break;
+                case "2":{
+
+                }
+                break;
+                case "3":{
+
+                }
+                break;
+                case "4":{
+
+                }
+                break;
+                case "5":{
+
+                }
+                break;
+                case "6":{
+
+                }
+                break;
+                case "7":{
+
+                }
+                break;
+                case "8":{
+
+                }
+                break;
+                case "9":{
+
+                }
+                break;
+                case "10":{
+
+                }
+                break;
+                case "11":{
+
+                }
+                break;
+            }
+        }
+    }
+}
