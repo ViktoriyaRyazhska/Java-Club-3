73878f8ccdf3fd72aea1e9d237620e84e910962b
olehbilykh <olehbilykh@gmail.com>
2021-10-06 20:15:51 +0300

    3rd push

--- src/com/softserve/team1/Main.java
+++ src/com/softserve/team1/Main.java
@@ -3,10 +3,6 @@ package com.softserve.team1;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("team1");
-        System.out.println("team1");
-        System.out.println("team1");
-
 
     }
 }
