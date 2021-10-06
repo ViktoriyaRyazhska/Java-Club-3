1f142d6bc90c5416b47659d04cd412741687d17d
Oleh8 <oleg.busygin@gmail.com>
2021-10-06 18:16:30 +0300

    Just testing

--- src/com/softserve/team1/Main.java
+++ src/com/softserve/team1/Main.java
@@ -2,6 +2,11 @@ package com.softserve.team1;
 
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("team1");
+        System.out.println("team1");
+        System.out.println("team1");
+
+
     }
 }
