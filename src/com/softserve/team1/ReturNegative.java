56da9e340013fd5d248c160ad3a272a8f78e1069
Oleh8 <oleg.busygin@gmail.com>
2021-10-07 17:35:16 +0300

    smth

--- src/com/softserve/team1/ReturNegative.java
+++ src/com/softserve/team1/ReturNegative.java
@@ -2,6 +2,6 @@ package com.softserve.team1;
 
 public class ReturNegative {
     public static int makeNegative(final int x) {
-        return x > 0 ?  x * -1: x;
+        return x > 0 ? -x : x;
     }
 }
