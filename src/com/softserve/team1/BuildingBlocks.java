f869bc1fbaa552691428d4dbcc11bf93bbbf04ec
Oleh8 <oleg.busygin@gmail.com>
2021-10-07 18:45:21 +0300

    done blocks ex

--- src/com/softserve/team1/BuildingBlocks.java
+++ src/com/softserve/team1/BuildingBlocks.java
@@ -1,5 +1,35 @@
 package com.softserve.team1;
 
 public class BuildingBlocks {
-    
+    public class Block {
+        private int width;
+        private int length;
+        private int height;
+
+        public Block(int [] arr){
+            this.width = arr[0];
+            this.length = arr[1];
+            this.height = arr[2];
+        }
+
+        public int getWidth() {
+            return width;
+        }
+
+        public int getLength() {
+            return length;
+        }
+
+        public int getHeight() {
+            return height;
+        }
+
+        public int getVolume() {
+            return height * width * length;
+        }
+
+        public int getSurfaceArea(){
+            return (height * length + height * width + width * length) * 2;
+        }
+    }
 }
