7b2202db84f7de01f00661a7c02c7d4db3a0a834
Taras Horodchuk <taras.gorodchuk.1999@gmail.com>
2021-10-11 02:42:04 +0300

    Added file PushAnOvjectIntoArray.java

---
+++ src/com/softserve/team1/PushAnObjectIntoArray.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class PushAnObjectIntoArray {
+    public static List<String> push(){
+        List<String> items = new ArrayList<>();
+        items.add("an object");
+        return items;
+    }
+}
