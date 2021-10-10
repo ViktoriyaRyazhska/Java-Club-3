31519ffbd2e012570d5f48afb9389422e6270675
Sorbexv1 <Sorbexv1@gmail.com>
2021-10-10 14:55:15 +0300

    Task 6 done by Sorbexv1

---
+++ src/com/softserve/team1/BasicOperations.java
@@ -0,0 +1,17 @@
+package com.softserve.team1;
+
+public class BasicOperations {
+    public static Integer basicMath(String op, int v1, int v2) {
+        switch (op) {
+            case "+":
+                return v1 + v2;
+            case "-":
+                return v1 - v2;
+            case "*":
+                return v1 * v2;
+            case "/":
+                return v1 / v2;
+        }
+        return 0;
+    }
+}
