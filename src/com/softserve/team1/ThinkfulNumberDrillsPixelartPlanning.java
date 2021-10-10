f06f759981751e1db46fc6a13947c42ad158f119
Sorbexv1 <Sorbexv1@gmail.com>
2021-10-10 14:36:38 +0300

    Task 1 by Sorbexv1

---
+++ src/com/softserve/team1/ThinkfulNumberDrillsPixelartPlanning.java
@@ -0,0 +1,9 @@
+package com.softserve.team1;
+
+public class ThinkfulNumberDrillsPixelartPlanning {
+
+    public static boolean isDivisible(int wallLength, int pixelSize) {
+        return wallLength % pixelSize == 0;
+
+    }
+}
