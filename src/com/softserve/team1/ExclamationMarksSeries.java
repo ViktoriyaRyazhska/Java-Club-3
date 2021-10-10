381e5c196d762d2d8178067a69d622622c6b90ba
Sorbexv1 <Sorbexv1@gmail.com>
2021-10-10 15:08:16 +0300

    Task 15 done by Sorbexv1

---
+++ src/com/softserve/team1/ExclamationMarksSeries.java
@@ -0,0 +1,11 @@
+package com.softserve.team1;
+
+public class ExclamationMarksSeries {
+
+    public static String remove(String s, int n) {
+        for (int i = 0; i < n; i++) {
+            s = s.replaceFirst("!", "");
+        }
+        return s;
+    }
+}
