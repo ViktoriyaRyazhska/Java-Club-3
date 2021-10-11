4e088e151951b4ff364a1fecdb40408f83bb6712
kqlynero <kqlynero@gmail.com>
2021-10-11 12:19:16 +0300

    mm

---
+++ team4/src/zub_denys/Divisible.java
@@ -0,0 +1,14 @@
+package zub_denys;
+
+import java.util.Scanner;
+
+public class Divisible {
+    public static boolean isDivisible(long n, long x, long y) {
+        return n % x == 0 && n % y == 0 ? true : false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isDivisible(10,2,5));
+        System.out.println(isDivisible(10,3,5));
+    }
+}
