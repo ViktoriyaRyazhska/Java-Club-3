5f967cb77c72d7128d5c06a8ae17b432490c1274
Natalia Kozak <nkozak4818@gmail.com>
2021-10-13 19:47:59 +0300

    One more test for Loops class

--- src/com/club3/java/tests/LoopsTest.java
+++ src/com/club3/java/tests/LoopsTest.java
@@ -1,10 +1,20 @@
 package com.club3.java.tests;
 
+
 import org.junit.Test;
-import static org.junit.Assert.assertArrayEquals;
+
 import static org.junit.Assert.assertEquals;
+
 
 public class LoopsTest {
     com.club3.java.impl.LoopsImpl loops = new com.club3.java.impl.LoopsImpl();
 
+
+    @Test
+    public void twiceAsOld() {
+        assertEquals(30, loops.twiceAsOld(30, 0));
+        assertEquals(16, loops.twiceAsOld(30, 7));
+        assertEquals(15, loops.twiceAsOld(45, 30));
+    }
 }
+
