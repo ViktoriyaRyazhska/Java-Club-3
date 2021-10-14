e9a275a3d723e67664909f9e39d97420180f29c4
Taras O <tarikogeryk@gmail.com>
2021-10-14 12:43:27 +0300

    Test for Conditions

--- src/com/club3/java/Conditions.java
+++ src/com/club3/java/Conditions.java
@@ -14,18 +14,7 @@ public class Conditions {
         int n = scanner.nextInt();
         int x = scanner.nextInt();
         int y = scanner.nextInt();
-
-        if (n % x == 0 && n % y == 0) {
-            return "n = " + n + " is divisible on " + "x = " + x + " y = " + y;
-        } else if (n % x == 0 && n % y != 0) {
-            return "n = " + n + " is divisible on " + "x = " + x + " but not on y = " + y;
-        } else if (n % x != 0 && n % y == 0) {
-            return "n = " + n + " not divisible on " + "x = " + x + " but divisible on y = " + y;
-        } else if (n % x != 0 && n % y != 0) {
-            return "n = " + n + " not divisible on " + "x = " + x + " y = " + y;
-        }
-        return "Some problems";
-
+        return conditionsImpl.divinity19(n, x, y);
     }
 
     private String rps23() {
@@ -35,19 +24,7 @@ public class Conditions {
         String p1 = scanner.nextLine();
         System.out.println("Enter for 2 player paper or scissors or rock");
         String p2 = scanner.nextLine();
-        String p = "paper";
-        String s = "scissors";
-        String r = "rock";
-
-
-        if (p1.equals(s) && p2.equals(p) || p1.equals(r) && p2.equals(s) || p1.equals(p) && p2.equals(r)) {
-            return "Player 1 won!";
-        } else if (p2.equals(s) && p1.equals(p) || p2.equals(r) && p1.equals(s) || p2.equals(p) && p1.equals(r)) {
-            return "Player 2 won!";
-        } else if (p1.equals(p2)) {
-            return "Draw!";
-        }
-        return "Ups!";
+        return conditionsImpl.rps23(p1, p2);
     }
 
     private int enough27() {
@@ -57,22 +34,10 @@ public class Conditions {
                 "If there is enough space, return 0, and if there isn't, return the number of passengers he can't take.");
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter cap,on,wait");
-
         int cap = scanner.nextInt();
         int on = scanner.nextInt();
         int wait = scanner.nextInt();
-
-        int sum = 0;
-        sum = on + wait;
-
-        if (cap >= sum) {
-            System.out.print("Enoughth plase empty : ");
-            return cap - sum;
-        } else if (cap - sum < 0) {
-            System.out.print("Not enoughth plase on : ");
-            return Math.abs(cap - sum);
-        }
-        return 000;
+        return conditionsImpl.enough27(cap, on, wait);
     }
 
     private int rentalCarCost31() {
@@ -81,39 +46,7 @@ public class Conditions {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter for how many days ");
         int d = scanner.nextInt();
-
-        int coast = 0;
-        int days = 1;
-        if (d < 3 && d > 0) {
-            for (int i = 0; i < d; i++) {
-                coast += 40;
-                System.out.println("day: " + days + " prise :" + coast);
-                days++;
-            }
-        } else if (d < 7 && d >= 3) {
-            for (int i = 0; i < d; i++) {
-                coast += 40;
-                if (i == 2) {
-                    coast -= 20;
-                }
-                System.out.println("day: " + days + " prise :" + coast);
-                days++;
-            }
-        }
-        if (d >= 7) {
-            for (int i = 0; i < d; i++) {
-                coast += 40;
-                if (i == 2) {
-                    coast -= 20;
-                }
-                if (i == 6) {
-                    coast -= 30;
-                }
-                System.out.println("day: " + days + " prise :" + coast);
-                days++;
-            }
-        }
-        return coast;
+        return conditionsImpl.rentalCarCost31(d);
     }
 
     private int oppositeNumber() {
