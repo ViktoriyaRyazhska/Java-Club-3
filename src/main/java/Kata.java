9bf4cd2fb275bc4efae2ca3398ef9cc08a997810
Viacheslav <77398514+viacheslav04@users.noreply.github.com>
2021-10-18 18:26:26 +0300

    Revert "added katas solutions and tests for them"

--- src/main/java/Kata.java
+++
@@ -1,8 +0,0 @@
-public abstract class Kata {
-    String title;
-    abstract String getTitle();
-    abstract String getDetails();
-    abstract boolean isValueCorrect(String input);
-    abstract String main(String input);
-
-}
--- src/test/java/ApplicationTest.java
+++
@@ -1,9 +0,0 @@
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
-
-public class ApplicationTest {
-    @Test
-    void main(){
-
-    }
-}
