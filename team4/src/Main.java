bf0916bb103f3aeb9dbd5625e24768dd2976be08
unknown <iill44iikk@gmail.com>
2021-10-18 13:20:11 +0300

    team4

--- team4/src/Main.java
+++ team4/src/Main.java
@@ -1,12 +1,7 @@
 
-<<<<<<< HEAD
 import svyshcho_illya.BasicTask1;
-
-
-
-=======
 import andrii_yaremko.*;
->>>>>>> 72c554c6604b9ed8c20718b5cfc2f7cd94b030b5
+
 
 import java.util.Scanner;
 
@@ -36,9 +31,10 @@ public class Main {
                 "73) filterOddNumber");
 
         while (true) {
-<<<<<<< HEAD
+
             System.out.println("Enter task's number:");
             Scanner myObj = new Scanner(System.in);
+
             switch (sc.next()) {
 
 
@@ -118,7 +114,7 @@ public class Main {
                 }
                 break;
                 //22--
-                case "16666": {
+                case "22": {
                     BasicTask1 task = new BasicTask1();
                     task.CalculateAge();
                     System.out.println("READY");
@@ -281,97 +277,96 @@ public class Main {
 
 
 
-=======
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("Enter task's number:");
-            switch (sc.next()) {
+
+
+
                 case "3": {
                     Task3 task3 = new Task3();
                     System.out.println("Enter length,width,height ");
-                    double lenght = scanner.nextDouble();
-                    double wigth = scanner.nextDouble();
-                    double height = scanner.nextDouble();
+                    double lenght = myObj.nextDouble();
+                    double wigth = myObj.nextDouble();
+                    double height = myObj.nextDouble();
                     task3.getVolumeOfCubiod(lenght,wigth,height);
                 }
                 break;
                 case "6": {
                     Task6 task6 = new Task6();
                     System.out.println("Please enter a number");
-                    int a = scanner.nextInt();
+                    int a = myObj.nextInt();
                     task6.to_binary(a);
                 }
                 break;
                 case "9": {
                     Task9 task9 = new Task9();
                     System.out.println("Please enter a number");
-                    double a = scanner.nextDouble();
+                    double a = myObj.nextDouble();
                     task9.makeNegative(a);
                 }
                 break;
                 case "12": {
                     System.out.println("Enter name and second name");
-                    int id = scanner.nextInt();
-                    String secondName = scanner.nextLine();
+                    int id = myObj.nextInt();
+                    String secondName = myObj.nextLine();
                     Task12 task12 = new Task12(id,secondName);
                     System.out.println(task12);
                 }
                 case "15": {
                     System.out.println("Enter width and height");
-                    int width = scanner.nextInt();
-                    int height = scanner.nextInt();
+                    int width = myObj.nextInt();
+                    int height = myObj.nextInt();
                     Task15 task15 = new Task15(width,height);
                     System.out.println("Draw enter x1 x2 x3 x4");
-                    int x1 = scanner.nextInt();
-                    int x2 = scanner.nextInt();
-                    int x3 = scanner.nextInt();
-                    int x4 = scanner.nextInt();
+                    int x1 = myObj.nextInt();
+                    int x2 = myObj.nextInt();
+                    int x3 = myObj.nextInt();
+                    int x4 = myObj.nextInt();
                     task15.draw(x1,x2,x3,x4);
                 }
                 break;
                 case "18": {
                     Task18 task18 = new Task18();
                     System.out.println("Enter your number");
-                    double a = scanner.nextDouble();
+                    double a = myObj.nextDouble();
                     task18.opposite(a);
                 }
                 case "21": {
                     Task21 task21 = new Task21();
                     System.out.println("Enter worker salary");
-                    int a = scanner.nextInt();
+                    int a = myObj.nextInt();
                     System.out.println("Enter bonuse true/false");
-                    boolean b = scanner.nextBoolean();
+                    boolean b = myObj.nextBoolean();
                     task21.addSalaryAndBonuses(a,b);
                 }
                 break;
                 case "24": {
                     Task24 task24 = new Task24();
                     System.out.println("Enter you number");
-                    double a = scanner.nextDouble();
+                    double a = myObj.nextDouble();
                     System.out.println("Enter operation + or - or * or /");
-                    String operation = scanner.next();
+                    String operation = myObj.next();
                     System.out.println("Enter you number");
-                    double b = scanner.nextDouble();
+                    double b = myObj.nextDouble();
                     task24.operations(a,b,operation);
                 }
                 break;
                 case "29": {
                     Task29 task29 = new Task29();
                     System.out.println("Enter nomber");
-                    String b  = scanner.next();
+                    String b  = myObj.next();
                     task29.enough(b);
                 }
                 break;
                 case "33": {
                     Task33 task339 = new Task33();
                     System.out.println("Enter human year");
-                    int ageHuman = scanner.nextInt();
+                    int ageHuman = myObj.nextInt();
                     task339.humanYearsCatYearsDogYears(ageHuman);
                 }
                 break;
                 case "37": {
                     Task37 task37 = new Task37();
                     System.out.println("Enter number");
-                    int a = scanner.nextInt();
+                    int a = myObj.nextInt();
                     task37.reverse(a);
                 }
                 break;
@@ -382,11 +377,11 @@ public class Main {
                 case "45": {
                     Task45 task45 = new Task45();
                     System.out.println("Enter Gap");
-                    int g = scanner.nextInt();
+                    int g = myObj.nextInt();
                     System.out.println("Enter start number");
-                    long m = scanner.nextLong();
+                    long m = myObj.nextLong();
                     System.out.println("Enter finish number");
-                    long n = scanner.nextLong();
+                    long n = myObj.nextLong();
                     task45.gap(g,m,n);
 
                 }
@@ -394,71 +389,70 @@ public class Main {
                 case "49": {
                     Task49 task49 = new Task49();
                     System.out.println("Enter source");
-                    String source = scanner.next();
+                    String source = myObj.next();
                     task49.encode(source);
                 }
                 break;
                 case "53": {
                     Task53 task53 = new Task53();
                     System.out.println("Enter name");
-                    String name = scanner.next();
+                    String name = myObj.next();
                     System.out.println("Enter city");
-                    String city = scanner.next();
+                    String city = myObj.next();
                     System.out.println("Enter state");
-                    String state = scanner.next();
+                    String state = myObj.next();
                     task53.sayHello(name,city,state);
                 }
                 break;
                 case "57": {
                     Task57 task57 = new Task57();
                     System.out.println("Enter words");
-                    String x = scanner.nextLine();
+                    String x = myObj.nextLine();
                     task57.noSpace(x);
                 }
                 break;
                 case "61": {
                     Task61 task61 = new Task61();
                     System.out.println("Enter letter");
-                    String alphabet = scanner.next();
+                    String alphabet = myObj.next();
                     task61.position(alphabet);
                 }
                 break;
                 case "65": {
                     Task65 task65 = new Task65();
                     System.out.println("Eneter Name and secondName");
-                    String name = scanner.nextLine();
+                    String name = myObj.nextLine();
                     task65.abbrevName(name);
                 }
                 break;
                 case "69": {
                     Task69 task69 = new Task69();
                     System.out.println("Enter text");
-                    String string = scanner.nextLine();
+                    String string = myObj.nextLine();
                     task69.toAlternativeString(string);
                 }
                 break;
                 case "73": {
                     Task73 task73 = new Task73();
                     System.out.println("Enter 5 numbers");
-                    int a = scanner.nextInt();
-                    int b = scanner.nextInt();
-                    int c = scanner.nextInt();
-                    int d = scanner.nextInt();
-                    int i = scanner.nextInt();
+                    int a = myObj.nextInt();
+                    int b = myObj.nextInt();
+                    int c = myObj.nextInt();
+                    int d = myObj.nextInt();
+                    int i = myObj.nextInt();
                     task73.filterOddNumber(a,b,c,d,i);
                 }
                 break;
 
->>>>>>> 72c554c6604b9ed8c20718b5cfc2f7cd94b030b5
-            }
+       }
 
 
         }
     }
-<<<<<<< HEAD
+
 }
 
 
-=======
-}
->>>>>>> 72c554c6604b9ed8c20718b5cfc2f7cd94b030b5
+
+
+
