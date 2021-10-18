f7a6951500e8bbf23e87fe1886611257b2a020c6
Oleh8 <oleg.busygin@gmail.com>
2021-10-18 16:43:46 +0300

    tests done

--- src/test/java/impl/BuildingBlocksTest.java
+++ src/test/java/impl/BuildingBlocksTest.java
@@ -1,6 +1,8 @@
 package impl;
 
+import com.softserve.team1.kata.impl.BuildingBlocks;
 import org.junit.jupiter.api.Test;
+import org.testng.Assert;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -8,6 +10,7 @@ class BuildingBlocksTest {
 
     @Test
     void test1() {
+
 
     }
 }
