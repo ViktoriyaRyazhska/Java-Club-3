b245f5a2154f684683f05ab8312056fb4db912e7
BorysOlkhovyk <borys.olkhovyk.pf.2017@lpnu.ua>
2021-10-18 12:03:50 +0300

    Created a new branch for Team 5

---
+++ JC_Team5/src/main/java/com/softserve/Java_club_team_5/App.java
@@ -0,0 +1,10 @@
+package com.softserve.Java_club_team_5;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Team 5!" );
+    }
+}
---
+++ JC_Team5/src/test/java/com/softserve/Java_club_team_5/AppTest.java
@@ -0,0 +1,20 @@
+package com.softserve.Java_club_team_5;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
