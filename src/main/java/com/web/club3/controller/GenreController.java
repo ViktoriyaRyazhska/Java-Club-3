0e22f4a52abd845cc463723352336cfdc457c614
Anastasiia Smilka <smilka_nastya@ukr.net>
2021-11-14 16:02:46 +0200

    edited GenreController

--- src/main/java/com/web/club3/controller/GenreController.java
+++ src/main/java/com/web/club3/controller/GenreController.java
@@ -1,17 +1,20 @@
 package com.web.club3.controller;
 
+import com.web.club3.model.Book;
 import com.web.club3.model.Genre;
 import com.web.club3.service.impl.GenreServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.List;
 
 @Controller
-@RequestMapping(name = "/genre")
+@RequestMapping("/genre")
 public class GenreController {
     private final GenreServiceImpl genreService;
 
@@ -20,10 +23,16 @@ public class GenreController {
         this.genreService = genreService;
     }
 
-    @RequestMapping(name = "/all", method = RequestMethod.GET)
+    @RequestMapping(method = RequestMethod.GET)
+    public String showGenrePage() {
+        return "genre/genre";
+    }
+
+    @GetMapping("/all")
     public String showAllGenres(Model model) {
         List<Genre> genres = genreService.findAll();
         model.addAttribute("genreModel", genres);
         return "genre/all";
     }
+
 }
